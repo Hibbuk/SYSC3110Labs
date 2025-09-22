40b2342cf28df4e94095d2b989882c73ae0cb4c2
Shael Kotecha <74563598+Hibbuk@users.noreply.github.com>
2025-09-22 15:40:31 -0400

    Add files via upload

---
+++ LAB1/src/AddressBook.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddies;
+
+    public AddressBook() {
+        buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            buddies.add(buddy);
+        }
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        buddies.remove(buddy);
+    }
+
+    public static void main(String[] args) {
+
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
+}
